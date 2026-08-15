23c15c794dbb4aafa0b1d9e878cef3696bb42c1e
Vlad Sandu <sanduvlad.dev@gmail.com>
2026-08-15 19:41:26 +0300

    feat: load application configuration

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"expense-tracker/internal/config"
+	"fmt"
+	"log"
+)
 
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(cfg.Server.Port)
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -33,18 +33,65 @@ func Load() (Config, error) {
 		return Config{}, fmt.Errorf("load .env: %w", err)
 	}
 
-	port, err := strconv.Atoi(os.Getenv("SERVER_PORT"))
+	serverPortStr, err := getEnv("SERVER_PORT")
+	if err != nil {
+		return Config{}, err
+	}
+
+	port, err := strconv.Atoi(serverPortStr)
 	if err != nil {
 		return Config{}, fmt.Errorf("parse SERVER_PORT: %w", err)
 	}
 
-	host := os.Getenv("DB_HOST")
+	host, err := getEnv("DB_HOST")
+	if err != nil {
+		return Config{}, err
+	}
+
+	dbPortStr, err := getEnv("DB_PORT")
+	if err != nil {
+		return Config{}, err
+	}
+
+	dbPort, err := strconv.Atoi(dbPortStr)
+	if err != nil {
+		return Config{}, fmt.Errorf("parse DB_PORT: %w", err)
+	}
+	user, err := getEnv("DB_USER")
+	if err != nil {
+		return Config{}, err
+	}
+
+	password, err := getEnv("DB_PASSWORD")
+	if err != nil {
+		return Config{}, err
+	}
+
+	name, err := getEnv("DB_NAME")
+	if err != nil {
+		return Config{}, err
+	}
+
 	return Config{
 		Server: ServerConfig{
 			Port: port,
 		},
 		Database: DatabaseConfig{
-			Host: host,
+			Host:     host,
+			Port:     dbPort,
+			User:     user,
+			Password: password,
+			Name:     name,
 		},
 	}, nil
+}
+
+func getEnv(key string) (string, error) {
+	value := os.Getenv(key)
+
+	if value == "" {
+		return "", fmt.Errorf("environment variable %s is required", key)
+	}
+
+	return value, nil
 }
