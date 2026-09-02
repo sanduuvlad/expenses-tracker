879ed4052081d252d782a598905276850d34b5a0
Vlad Sandu <sanduvlad.dev@gmail.com>
2026-09-02 10:54:18 +0300

    add postgres database connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"expense-tracker/internal/config"
+	"expense-tracker/internal/database"
 	"fmt"
 	"log"
 )
@@ -12,5 +13,19 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(cfg.Server.Port)
+	dsn := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s",
+		cfg.Database.Host,
+		cfg.Database.Port,
+		cfg.Database.User,
+		cfg.Database.Password,
+		cfg.Database.Name,
+	)
+
+	db, err := database.Open(dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
 }
