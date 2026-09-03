68165c52cd4061d5847f15fc1376cca56b2ae2b2
Vlad Sandu <sanduvlad.dev@gmail.com>
2026-09-03 16:44:42 +0300

    add GetAllUsers method

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"database/sql"
+	"expense-tracker/internal/models"
 )
 
 type Repository struct {
@@ -12,4 +13,41 @@ func NewRepository(db *sql.DB) *Repository {
 	return &Repository{
 		db: db,
 	}
+}
+
+func (r *Repository) GetAllUsers() ([]models.User, error) {
+	rows, err := r.db.Query(`
+		SELECT id, email, password_hash, created_at, updated_at
+		FROM users
+	`)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var users []models.User
+
+	for rows.Next() {
+		var user models.User
+
+		err := rows.Scan(
+			&user.ID,
+			&user.Email,
+			&user.PasswordHash,
+			&user.CreatedAt,
+			&user.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		users = append(users, user)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return users, nil
 }
